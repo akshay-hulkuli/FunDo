b04e27f20043f14ed2cae8aabeff27f9f9e8d951
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-09 11:01:07 +0530

    [Akshay]ADDED: method to validate data and to axios api call

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -3,8 +3,74 @@ import './signup.scss';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import UserService from '../../services/UserService';
+const userService = new UserService();
+
 
 export default class SignUp extends Component {
+    
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          fName: "",
+          lName: "",
+          userName: "",
+          password: "",
+          repeatedPsw: "",
+          fNameError: false,
+          lNameError: false,
+          userNameError : false,
+          passwordError: false,
+          repeatedPswError: false,
+          redirect: null,
+        };
+      }
+
+    change = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value,
+        });
+    };
+
+    isValid = () => {
+        let isError = false;
+        const errors = this.state;
+
+        errors.fNameError = this.state.fName !== "" ? false : true;
+        errors.lNameError = this.state.lName !== "" ? false : true;
+        errors.userNameError = this.state.userName !== "" ? false: true;
+        errors.passwordError = this.state.password !== ""? false: true;
+        errors.repeatedPswError = this.state.repeatedPsw !== "" ? false: true;
+
+        this.setState({
+            ...errors
+        });
+
+        return (isError = errors.fNameError || errors.lNameError || errors.userNameError || errors.passwordError || errors.repeatedPswError)
+    };
+
+    next = () => {
+        var isValidated = this.isValid();
+        console.log(this.state)
+        if(!isValidated){
+            console.log("validation sucessfull");
+            let data = {
+                "firstName": this.state.fName,
+                "lastName": this.state.lName,
+                "email" : this.state.userName,
+                "password" : this.state.password,
+                "service": "advance"
+            }
+            userService.Registration("http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp", data)
+                .then(()=>{
+                    console.log("sucessfully registered");
+                })
+                .catch ((err)=> {
+                    console.log(err);
+                });
+        }
+    }
     render() {
         return (
             <div class="main-content">
@@ -17,14 +83,62 @@ export default class SignUp extends Component {
                     <span class="main-content-left-header">create your fundo account</span>
                     <form id="form">
                         <div class="name">
-                            <TextField fullWidth label="first name" id="firstName" size="small" margin="dense" sx={{marginRight:'5px'}}/>
-                            <TextField fullWidth label="last name" id="lastName" size="small" margin="dense" sx={{marginLeft:'5px'}}/>
+                            <TextField 
+                                fullWidth 
+                                label="first name" 
+                                name ="fName"
+                                error={this.state.fNameError}
+                                helperText = {this.state.fNameError ? "first name is req" : ""}
+                                onChange = {(e) => this.change(e)}
+                                size="small" 
+                                margin="dense" 
+                                sx={{marginRight:'5px'}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                label="last name" 
+                                name ="lName"
+                                error={this.state.lNameError}
+                                helperText = {this.state.lNameError ? "last name is req" : ""}
+                                onChange = {(e) => this.change(e)}
+                                size="small" 
+                                margin="dense" 
+                                sx={{marginLeft:'5px'}}/>
                         </div>
-                        <TextField fullWidth label="user name"  id="fullWidth" size="small" margin="dense" helperText="you can use numbers,letters and periods" />
+                        <TextField 
+                            fullWidth 
+                            label="user name"  
+                            name ="userName"
+                            error={this.state.userNameError}
+                            helperText = {this.state.userNameError ? "this is a required field" : "you can use numbers,letters and periods"}
+                            onChange = {(e) => this.change(e)}
+                            size="small" 
+                            margin="dense" 
+                        />
                         <div><p class="p">Use my current email address instead</p></div>
                         <div class="psw">
-                            <TextField fullWidth label="Password" id="psw" size="small" margin="dense" sx={{marginRight:'5px'}}/>
-                            <TextField fullWidth label="Confirm" id="pswRepeat" size="small" margin="dense" sx={{marginLeft:'5px'}}/>
+                            <TextField 
+                                fullWidth 
+                                label="Password" 
+                                name ="password"
+                                error={this.state.passwordError}
+                                helperText = {this.state.passwordError ? "password is req" : ""}
+                                onChange = {(e) => this.change(e)}
+                                size="small" 
+                                margin="dense" 
+                                sx={{marginRight:'5px'}}
+                            />
+                            <TextField 
+                                fullWidth 
+                                label="Confirm" 
+                                name ="repeatedPsw"
+                                error={this.state.repeatedPswError}
+                                helperText = {this.state.repeatedPswError ? "confirm the password" : ""}
+                                onChange = {(e) => this.change(e)}
+                                size="small" 
+                                margin="dense" 
+                                sx={{marginLeft:'5px'}}
+                            />
                         </div>
                         <p class="pswhelper">Use 8 or more characters with a mix of letters, numbers & symbols</p>
                         <div class="check">
@@ -32,7 +146,7 @@ export default class SignUp extends Component {
                         </div>
                        <div class="buttons">
                             <Button>Sign in instead</Button>
-                            <Button variant="contained">Next</Button>
+                            <Button variant="contained" onClick={this.next}>Next</Button>
                        </div>
                     </form>
                 </div>
