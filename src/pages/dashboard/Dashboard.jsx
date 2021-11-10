938675586737327074f39e97299604ba63719259
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-10 21:51:10 +0530

    [Akshay]REFACTORED: to display and add notes dynamically

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import './dashboard.scss'
 import DashBoardHeader from '../../components/header/Header'
 import NotesMaker from '../../components/notemaker/NotesMaker'
 import Box from '@mui/material/Box';
@@ -6,10 +7,15 @@ import { styled, useTheme, createTheme,ThemeProvider } from '@mui/material/style
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import DisplayNotes from '../../components/DisplayNotes/DisplayNotes';
+import Dialog from "@mui/material/Dialog";
+import NoteService from '../../services/NoteService';
+import DialogContent from '@mui/material/DialogContent';
+import EditNotes from '../../components/EditNotes/EditNotes';
+const noteService = new NoteService();
 
 export default function Dashboard() {
   const [dataArray, setDataArray] = useState([]);
-
+  const [editable, setEditable] = useState({});
   const theme = createTheme({
     palette:{
         mode:'dark',
@@ -18,6 +24,9 @@ export default function Dashboard() {
         }
     }
   });
+
+ 
+
   const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
@@ -32,21 +41,63 @@ export default function Dashboard() {
     color: theme.palette.text.secondary,
   }));
 
+  const getData = () => {
+    var config = {
+      headers:{
+          "Authorization" : localStorage.getItem('uid'),
+      }
+    }
+    noteService.getNotes('http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList', config)
+      .then((notes)=>{
+        // console.log(notes.data.data.data)
+        setDataArray(notes.data.data.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+  }
+
+  useEffect(()=>{
+    getData();
+    console.log('inside useEffect')
+  },[])
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = (data) => {
+    setOpen(true);
+    setEditable(data);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex', backgroundColor:'#202124' }}>
         <DashBoardHeader/>
         <Box Box component="main" sx={{ flexGrow: 1, p: 3}}>
           <DrawerHeader/>
-          <NotesMaker/>
+          <NotesMaker func={getData}/>
           <Box sx={{paddingTop:'5%'}}/>
           <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{md:10}}>
-            {dataArray.map((data)=>(
+            {dataArray.map((data, index)=>(
                 <Grid item xs={6} sm={3} md={2} >
-                <Item><DisplayNotes data={data}/></Item>
+                <Item><DisplayNotes data={data} dialog={handleClickOpen}/></Item>
               </Grid>
             ))}
           </Grid>
+          <Dialog
+            open={open}
+            onClose={handleClose} 
+          >
+            <Box sx={{width:'600px'}}>
+              <EditNotes onClose = {handleClose} data={editable}/>
+            </Box>
+            
+          </Dialog>
         </Box>
       </Box>
     </ThemeProvider>
