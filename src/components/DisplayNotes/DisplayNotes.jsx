360532a932752bc3ecc8c0bfac4881f9f17f13d4
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-10 21:49:02 +0530

    [Akshay]REFACTORED: to invoke dialog box based on click event

--- src/components/DisplayNotes/DisplayNotes.jsx
+++ src/components/DisplayNotes/DisplayNotes.jsx
@@ -1,16 +1,10 @@
-import { Button, Collapse, Icon, IconButton, Paper } from '@mui/material';
+import {IconButton, Paper, Typography } from '@mui/material';
 import React from 'react';
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
-import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
-import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
-import CropOriginalOutlinedIcon from '@mui/icons-material/CropOriginalOutlined';
-import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Icons from "../icons/Icons";
 
 export default function DisplayNotes(props) {
     const [checked, setChecked] = React.useState(false);
@@ -37,28 +31,26 @@ export default function DisplayNotes(props) {
         </Box>
     );
     const bottom = (
-        <Box sx={{display:'flex'}}>
-            <IconButton><AddAlertOutlinedIcon/></IconButton>
-            <IconButton><PersonAddAltOutlinedIcon/></IconButton>
-            <IconButton><CropOriginalOutlinedIcon/></IconButton>
-            <IconButton><ArchiveOutlinedIcon/></IconButton>
-            <IconButton><ColorLensOutlinedIcon/></IconButton>
-            <IconButton><MoreVertOutlinedIcon/></IconButton>
+        <Box>
+            <Icons/>
         </Box>
-    );
-    
+        
+    )
+    const handleDialog = () => {
+        props.dialog(props.data);
+    }
     return (
         <ThemeProvider theme={theme}>
-            <Box  sx={{display:'flex', flexDirection:'column'}} onMouseEnter={open} onMouseLeave={close}>
+            <Box  sx={{display:'flex', flexDirection:'column'}} onMouseEnter={open} onMouseLeave={close} >
                 <Paper sx={{border:'0.1px solid', borderRadius:'8px', padding: '0 20px'}} >
                     <Fade in={checked}>{title}</Fade>
-                    <Box sx={{fontSize:'18px', padding:'5px'}}>
-                        <div>
-                            {props.data.content}
+                    <Box sx={{fontSize:'18px', padding:'5px'}} onClick={handleDialog}>
+                        <div sx={{ wordBreak: 'break-all'}} id="container">
+                            {props.data.description}
                         </div>
                     </Box>
                     <Fade in={checked}>{bottom}</Fade>
-                </Paper>
+                </Paper>    
             </Box>
         </ThemeProvider>
     )
