8a1ea344818ad7078434c19edfb66cd3d2a19f84
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-13 20:45:12 +0530

    [Akshay]REFACTORED: to pass required props to editnote component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -8,7 +8,6 @@ import Paper from '@mui/material/Paper';
 import DisplayNotes from '../../components/DisplayNotes/DisplayNotes';
 import Dialog from "@mui/material/Dialog";
 import NoteService from '../../services/NoteService';
-import DialogContent from '@mui/material/DialogContent';
 import EditNotes from '../../components/EditNotes/EditNotes';
 const noteService = new NoteService();
 
@@ -19,8 +18,8 @@ export default function Dashboard() {
   const theme = createTheme({
     palette:{
         mode:colorTheme,
-        color1:{
-          main:'pink[500]'
+        primary:{
+          main:'#202124'
         } 
     }
   });
@@ -79,10 +78,10 @@ export default function Dashboard() {
           <DrawerHeader/>
           <NotesMaker func={getData}/>
           <Box sx={{paddingTop:'5%'}}/>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{md:10}}>
-            {dataArray.map((data, index)=>(
-                <Grid item xs={6} sm={3} md={2} >
-                <Item><DisplayNotes data={data} dialog={handleClickOpen}/></Item>
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{md:10, xs: 3}}>
+            {dataArray.filter(data => data.isArchived===false && data.isDeleted === false).map((data)=>(
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <Item><DisplayNotes data={data} dialog={handleClickOpen} getData={getData}/></Item>
               </Grid>
             ))}
           </Grid>
