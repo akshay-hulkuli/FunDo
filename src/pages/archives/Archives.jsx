1173bdbaa7ec4e62409add134aaec84759216b61
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-15 18:27:17 +0530

    [Akshay]REFACTORED: kept header outside archive component

--- src/pages/archives/Archives.jsx
+++ src/pages/archives/Archives.jsx
@@ -15,28 +15,7 @@ const noteService = new NoteService();
 export default function Archives() {
   const [dataArray, setDataArray] = useState([]);
   const [editable, setEditable] = useState({});
-  const [colorTheme, setcolorTheme] = useState('dark')
-  const theme = createTheme({
-    palette:{
-        mode:colorTheme,
-        primary:{
-          main:'#202124'
-        } 
-    }
-  });
 
-  const onThemeChange = () => {
-    setcolorTheme(colorTheme==='dark'?'light':'dark');
-  }
-
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  }));
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     textAlign: 'center',
@@ -72,26 +51,21 @@ export default function Archives() {
 
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box Box sx={{ display: 'flex'}}>
-        <DashBoardHeader onThemeChange={onThemeChange}/>
-        <Box Box component="main" sx={{ flexGrow: 1, p: 3}}>
-          <DrawerHeader/>
-          <Box sx={{paddingTop:'5%'}}/>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{md:10, xs: 3}}>
-            {dataArray.filter(data => data.isArchived=== true && data.isDeleted=== false).map((data)=>(
-                <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-                <Item><DisplayNotes data={data} dialog={handleClickOpen} getData={getData}/></Item>
-              </Grid>
-            ))}
-          </Grid>
-          <Dialog open={open} onClose={handleClose}>
-            <Box sx={{width:'600px'}}>
-              <EditNotes onClose = {handleClose} data={editable} updateData={getData}/>
-            </Box>
-            </Dialog>
-        </Box> 
-      </Box>
-    </ThemeProvider>
+      <React.Fragment>
+        <Box sx={{paddingTop:'5%'}}/>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{md:10, xs: 3}}>
+          {dataArray.filter(data => data.isArchived=== true && data.isDeleted=== false).map((data)=>(
+              <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+              <Item><DisplayNotes data={data} dialog={handleClickOpen} getData={getData}/></Item>
+            </Grid>
+          ))}
+        </Grid>
+        <Dialog open={open} onClose={handleClose}>
+          <Box sx={{width:'600px'}}>
+            <EditNotes onClose = {handleClose} data={editable} updateData={getData}/>
+          </Box>
+        </Dialog>
+      </React.Fragment>
+  
   )
 }
