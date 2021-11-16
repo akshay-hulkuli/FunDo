c48ab857a117a642165f65a76f7f247d5834d1c8
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-16 16:13:05 +0530

    [Akshay]DELETED: unwanted import statements

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,19 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState } from 'react'
 import DashBoardHeader from '../../components/header/Header'
 import Box from '@mui/material/Box';
-// import NotesMaker from '../../components/notemaker/NotesMaker'
-// import DisplayNotes from '../../components/DisplayNotes/DisplayNotes';
-// import Dialog from "@mui/material/Dialog";
-// import EditNotes from '../../components/EditNotes/EditNotes';
-// import Grid from '@mui/material/Grid';
-import NoteService from '../../services/NoteService';
-// import Paper from '@mui/material/Paper';
-import { styled, useTheme, createTheme,ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { styled, createTheme,ThemeProvider } from '@mui/material/styles';
+import { Route, Switch } from 'react-router-dom';
 import Home from '../home/Home';
 import Archives from '../archives/Archives';
 import Trash from '../trash/Trash';
-const noteService = new NoteService();
 
 export default function Dashboard() {
   const [colorTheme, setcolorTheme] = useState('dark');
@@ -49,9 +41,9 @@ export default function Dashboard() {
         <Box Box component="main" sx={{ flexGrow: 1, p: 3}}>
           <DrawerHeader/>
           <Switch>
-          <Route exact path="/dashboard" component={Home}/>
-          <Route exact  path="/dashboard/archive" component={Archives}/>
-          <Route exact path="/dashboard/trash" component={Trash}/>
+            <Route exact path="/dashboard" component={Home}/>
+            <Route exact  path="/dashboard/archive" component={Archives}/>
+            <Route exact path="/dashboard/trash" component={Trash}/>
           </Switch>
         </Box> 
       </Box>
