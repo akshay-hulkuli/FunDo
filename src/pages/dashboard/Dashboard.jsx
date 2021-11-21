71ff7b7a973fbc5293f80651654b9224b8409a15
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-21 18:04:26 +0530

    [Akshay]ADDED: usestate to hold notes array

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,7 +9,12 @@ import Trash from '../trash/Trash';
 
 export default function Dashboard() {
   const [colorTheme, setcolorTheme] = useState('dark');
+  const [dataArray, setDataArray] = useState([]);
+  const [backup, setBackup] = useState([]);
   
+  const callBackUp = (a) => {
+    setBackup(a);
+  }
   const theme = createTheme({
     palette:{
         mode:colorTheme,
@@ -37,11 +42,16 @@ export default function Dashboard() {
   return (
     <ThemeProvider theme={theme}>
       <Box Box sx={{ display: 'flex'}}>
-        <DashBoardHeader onThemeChange={onThemeChange}/>
+        <DashBoardHeader 
+          onThemeChange={onThemeChange} 
+          dataArray={dataArray} 
+          setDataArray={setDataArray}
+          backup = {backup}
+          setBackup={setBackup}/>
         <Box Box component="main" sx={{ flexGrow: 1, p: 3}}>
           <DrawerHeader/>
           <Switch>
-            <Route exact path="/dashboard" component={Home}/>
+            <Route exact path="/dashboard" render={()=><Home dataArray={dataArray} setDataArray={setDataArray} callBackUp ={callBackUp}/>}/>
             <Route exact  path="/dashboard/archive" component={Archives}/>
             <Route exact path="/dashboard/trash" component={Trash}/>
           </Switch>
